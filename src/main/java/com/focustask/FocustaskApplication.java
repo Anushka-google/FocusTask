4b82cd791e63bd5a2697b872bb6b1227dfd37450
Anushka <anushkakaushal2611@gmail.com>
2026-09-16 18:50:08 +0530

    feat: phase 6 - connect MySQL datasource and configure JPA Hibernate

--- src/main/java/com/focustask/FocustaskApplication.java
+++ src/main/java/com/focustask/FocustaskApplication.java
@@ -2,14 +2,8 @@ package com.focustask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 
-// DataSource is excluded temporarily until Phase 6 (MySQL configuration)
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class FocustaskApplication {
 
 	public static void main(String[] args) {
